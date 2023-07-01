c80ecbc032c79293d484b2b55da3437887a42cdf
Vasya <vskesha@gmail.com>
2023-07-01 22:39:38 +0300

    changed context_manager.py in 'module6'

--- module6/context_manager.py
+++ module6/context_manager.py
@@ -1,14 +1,18 @@
+def some_not_useful_function(f):
+    raise FileNotFoundError
+
+
 def main():
     fh = open('test.txt')
     try:
-        some_useful_function(fh)
+        some_not_useful_function(fh)
     except:
         print('inside except block')
     finally:
         fh.close()
 
     with open('test.txt') as fh:
-        some_useful_function(fh)
+        some_not_useful_function(fh)
 
 
 if __name__ == '__main__':
