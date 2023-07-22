2ade754359130f48f80752a578ff5bfa27482750
Vasya <vskesha@gmail.com>
2023-07-22 23:02:49 +0300

    added test.py in test_folder

---
+++ test_folder/test.py
@@ -0,0 +1,15 @@
+def data_preparation(list_data):
+    result = []
+    for lst in list_data:
+        if len(lst) < 3:
+            continue
+        lst.remove(max(list))
+        lst.remove(min(list))
+        result.extend(lst)
+    result.sort(reverse=True)
+    return result
+
+
+data_preparation([[1, 2, 3, 0], [3], [5, 6, 1, 7, 2]])
+# [[1, 2, 3, 0], [3], [5, 6, 1, 7, 2]] поверне наступний список [6, 5, 3, 2, 2, 1]
+data_preparation([[1, 2, 3, 0], [3], [5, 6, 1, 7, 2]])
